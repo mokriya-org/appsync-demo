27f5296074baa877c005a7103be3780440f73ccb
Christian Talmo <talmo.christian@gmail.com>
2018-08-31 07:44:43 +0300

    AWS AppSync config/credentials

---
+++ src/aws-appsync-config.js
@@ -0,0 +1,6 @@
+﻿module.exports = {
+  'graphqlEndpoint': 'https://ulescl3be5c3fdcrprk5fcj33u.appsync-api.us-east-2.amazonaws.com/graphql',
+  'region': 'us-east-2',
+  'authenticationType': 'API_KEY',
+  'apiKey': 'da2-7t2k2sroe5exrgvxvikkqx4acq'
+}
