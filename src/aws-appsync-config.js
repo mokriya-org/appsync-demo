c7ba8afb3a91abbacb7102755c5a357d73a602b7
Christian Talmo <talmo.christian@gmail.com>
2018-09-09 04:34:12 +0300

    switched to new HttpChatDemo AWS AppSync Endpoint made by Alex

--- src/aws-appsync-config.js
+++ src/aws-appsync-config.js
@@ -1,6 +1,6 @@
 ﻿module.exports = {
-  'graphqlEndpoint': 'https://ulescl3be5c3fdcrprk5fcj33u.appsync-api.us-east-2.amazonaws.com/graphql',
+  'graphqlEndpoint': 'https://cpp6ajvbfjbsnp7q4sp2carifu.appsync-api.us-east-2.amazonaws.com/graphql',
   'region': 'us-east-2',
   'authenticationType': 'API_KEY',
-  'apiKey': 'da2-7t2k2sroe5exrgvxvikkqx4acq'
+  'apiKey': 'da2-2rghn3e4a5ahve4nayakhkmlxe'
 }
